d58407ef57a3575f1bf6de2e8c323d6b0d7c7fec
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-03-01 15:16:22 -0600

    Add proper debug

--- web.py
+++ web.py
@@ -5,6 +5,7 @@ import requests as r
 app = Flask(__name__)
 
 NOMIE_API_KEY = os.environ.get("NOMIE_API_KEY")
+print NOMIE_API_KEY
 nomie_api_head = "https://api.nomie.io/v2/"
 
 @app.route("/")
@@ -17,9 +18,11 @@ def parse_request():
     rawPriority = request.form['priority']
     priority = rawPriority[9:]
     project = request.form['project']
-    r.get(nomie_api_head + '/push/' + NOMIE_API_KEY + '/label=' + project + '/value=' + priority)
+    url = nomie_api_head + '/push/' + NOMIE_API_KEY + '/label=' + project + '/value=' + priority
+    print url
+    r.get(url)
     return '{"status": "success"}'
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 1492))
     app.run(host='0.0.0.0', port=port, debug=True)
