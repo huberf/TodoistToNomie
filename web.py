a9b7e7cc2a6e69290d3420b5b3e1adfca1704b91
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-03-05 17:32:05 -0600

    Modularize functionality

--- web.py
+++ web.py
@@ -10,6 +10,15 @@ TODOIST_USER_TOKEN = os.environ.get('TODOIST_USER_TOKEN')
 print NOMIE_API_KEY
 nomie_api_head = "https://api.nomie.io/v2/"
 
+def load_project(project_id):
+    project_data = json.loads(r.get('https://todoist.com/API/v7/projects/get?token=' + TODOIST_USER_TOKEN + '&project_id=' + project_id).text)
+    return project_data
+
+def tap_tracker(name, value):
+    url = nomie_api_head + 'push/' + NOMIE_API_KEY + '/action=track/label=' + name + '/value=' + value
+    print url
+    r.get(url)
+
 @app.route("/")
 def main():
     return "I'm online"
@@ -20,11 +29,9 @@ def parse_request():
         data = request.json['event_data']
         priority = str(data['priority'])
         project_id = str(data['project_id'])
-        project_data = json.loads(r.get('https://todoist.com/API/v7/projects/get?token=' + TODOIST_USER_TOKEN + '&project_id=' + project_id).text)
+        project_data = load_project(project_id)
         project = project_data['project']['name']
-        url = nomie_api_head + 'push/' + NOMIE_API_KEY + '/action=track/label=' + project + '/value=' + priority
-        print url
-        r.get(url)
+        tap_tracker(project, priority)
     return '{"status": "success"}'
 
 if __name__ == "__main__":
